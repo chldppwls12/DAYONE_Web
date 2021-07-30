b2b2acf8774be16854dae1b9d72132111089dc84
chldppwls12 <chldppwls12@naver.com>
2021-07-31 02:08:19 +0900

    calendarpage model 수정

--- BackEnd/DAYONE_web/calendarpage/admin.py
+++ BackEnd/DAYONE_web/calendarpage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Reservation, TeamMember
+from .models import Reservation
 
 # Register your models here.
 @admin.register(Reservation)
@@ -7,7 +7,3 @@ class ReservationAdmin(admin.ModelAdmin):
   list_display = [
     'pk', 'representative', 'content',
     'date', 'start_time', 'end_time']
-
-@admin.register(TeamMember)
-class TeamMember(admin.ModelAdmin):
-  pass
--- BackEnd/DAYONE_web/calendarpage/models.py
+++ BackEnd/DAYONE_web/calendarpage/models.py
@@ -5,15 +5,12 @@ from django.conf import settings
 
 #settings에 AUTH_USER_MODEL 설정하기
 class Reservation(models.Model):
-  representative = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  representative = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='representative_reservation')
   content = models.CharField(max_length=200, blank=True)
   date = models.DateField()
   start_time = models.TimeField()
   end_time = models.TimeField()
+  member = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='member_reservation')
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   is_deleted = models.BooleanField(default=False)
-
-class TeamMember(models.Model):
-  reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-  member = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
