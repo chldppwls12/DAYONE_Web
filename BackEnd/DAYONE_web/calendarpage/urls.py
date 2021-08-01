9afb0e44b6a76a8a6e5b69a3689a770a3f6a8400
chldppwls12 <chldppwls12@naver.com>
2021-08-01 11:17:42 +0900

    Feat #25 예약 추가 기능 (유효성 검사 X)

--- BackEnd/DAYONE_web/calendarpage/urls.py
+++ BackEnd/DAYONE_web/calendarpage/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "calendarpage"
 urlpatterns = [
   path('', views.index, name='index'),
+  path('new/', views.new, name='new'),
 ]
--- BackEnd/DAYONE_web/calendarpage/views.py
+++ BackEnd/DAYONE_web/calendarpage/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from .models import Reservation
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
   return render(request, 'calendarpage/index.html')
+
+def new(request):
+  current_user = request.user
+  members = get_user_model().objects.exclude(id=current_user.id)
+  if request.method == 'GET':
+    return render(request, 'calendarpage/reservation.html', {'members': members})
+  else:
+    reservation = Reservation()
+    reservation.representative = current_user
+    reservation.content = request.POST['content']
+    reservation.date = request.POST['date']
+    reservation.start_time = request.POST['start_time']
+    reservation.end_time = request.POST['end_time']
+    add_members = request.POST.getlist('members')
+    add_members_obj = []
+    for member in add_members:
+      add_members_obj.append(members.filter(username=member))
+    if int(reservation.start_time.split(':')[0]) < 12:
+      time_category = '오전'
+    else:
+      time_category = '오후'
+    print(add_members_obj)
+    reservation.save()
+    for member in add_members_obj:
+      reservation.member.add(members.get(id=member[0].id))
+    return render(request, 'calendarpage/reservationCheck.html', {'reservation': reservation, 'time_category': time_category})
